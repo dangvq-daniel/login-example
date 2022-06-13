34a80e9ae3f1119ca55c8b4899caa7539cbb5cde
dangvq-daniel <57842685+dangvq-daniel@users.noreply.github.com>
2022-06-13 13:49:14 -0400

    Initial commit

--- main.py
+++ main.py
@@ -59,42 +59,35 @@ def register():
     # Output message if something goes wrong...
     msg = ''
     # Check if "username", "password" and "email" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form and 'token' in request.form:
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
         # Create variables for easy access
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
-        token = request.form['token']
-        # Check if account exists using MySQL
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
-        account = cursor.fetchone()
-        # If account exists show error and validation checks
-        if account:
-            msg = 'Account already exists!'
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            msg = 'Invalid email address!'
-        elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers!'
-        elif not username or not password or not email:
-            msg = 'Please fill out the form!'
-        else:
-            cursor.execute("SELECT * FROM tokens WHERE token LIKE '{0}'".format(token))
-            verification = cursor.fetchone()
-            if verification:
-                # Account doesnt exists and the form data is valid, now insert new account into accounts table
-                cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
-                mysql.connection.commit()
-                msg = 'You have successfully registered!'
-                return redirect(url_for('login'))
-            else:
-                msg = 'Invalid token'
     elif request.method == 'POST':
         # Form is empty... (no POST data)
         msg = 'Please fill out the form!'
     # Show registration form with message (if any)
     return render_template('register.html', msg=msg)
-
+    email = request.form['email']
+    # Check if account exists using MySQL
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+    account = cursor.fetchone()
+    # If account exists show error and validation checks
+    if account:
+        msg = 'Account already exists!'
+    elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+        msg = 'Invalid email address!'
+    elif not re.match(r'[A-Za-z0-9]+', username):
+        msg = 'Username must contain only characters and numbers!'
+    elif not username or not password or not email:
+        msg = 'Please fill out the form!'
+    else:
+        # Account doesnt exists and the form data is valid, now insert new account into accounts table
+        cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
+        mysql.connection.commit()
+        msg = 'You have successfully registered!'
 
 # http://localhost:5000/pythinlogin/home - this will be the home page, only accessible for loggedin users
 @app.route('/pythonlogin/home')
@@ -119,3 +112,6 @@ def profile():
         return render_template('profile.html', account=account)
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))
+
+if __name__ == '__main__':
+ app.run(debug=True)
