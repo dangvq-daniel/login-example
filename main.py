4a6c45ac790dd7df4b1a26250735bb537f6dd0b8
dangvq-daniel <57842685+dangvq-daniel@users.noreply.github.com>
2022-06-22 22:27:17 -0400

    Fix url for heroku url

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app.config['MYSQL_DB'] = 'pythonlogin'
 mysql = MySQL(app)
 
 # http://localhost:5000/pythonlogin/ - the following will be our login page, which will use both GET and POST requests
-@app.route('/pythonlogin/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
     # Output message if something goes wrong...
     msg = ''
